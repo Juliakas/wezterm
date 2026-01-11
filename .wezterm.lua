04cdc067d6bac58ac7fae65078facc8ae5bc545b
Julius <julius.rupsys@bentley.com>
2026-01-11 17:18:46 +0200

    initial config

---
+++ .wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.keys = {}
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 12
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+
+config.allow_win32_input_mode = false
+config.default_prog = { "pwsh" }
+
+for i = 1, 8 do
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
+
+-- Finally, return the configuration to wezterm:
+return config
